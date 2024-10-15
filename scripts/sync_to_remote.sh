ab7c3470e455babc7740c72b55cc1c45b51741d7
Matej Hornik <bachini256@gmail.com>
2024-10-15 13:56:29 +0200

    fix sync script

--- scripts/sync_to_remote.sh
+++ scripts/sync_to_remote.sh
@@ -5,17 +5,17 @@
 LOCAL_DIR="dp_mit"
 REMOTE_USER="xhorni20"
 REMOTE_HOST="skirit.ics.muni.cz"
-REMOTE_DIR="/storage/brno2/home/xhorni20"
-EXCLUDES=(".venv" ".git")
+REMOTE_DIR="/storage/brno2/home/xhorni20/dp_mit"
+EXCLUDES=(".venv" "wandb" ".git")
 
 # Construct the rsync exclude options
 EXCLUDE_OPTS=""
 for EXCLUDE in "${EXCLUDES[@]}"; do
-    EXCLUDE_OPTS+="--exclude=$EXCLUDE "
+    EXCLUDE_OPTS+="--exclude=${EXCLUDE} "
 done
 
-# Execute rsync with exclusion and overwrite existing files on remote server
-rsync -av --delete $EXCLUDE_OPTS $LOCAL_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
+# Execute rsync with exclusion and overwrite existing files on remote server without deleting files in the destination directory
+rsync -av ${EXCLUDE_OPTS} ${LOCAL_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
 
 # Output message
 echo "Sync complete. Excluded directories: ${EXCLUDES[*]}"
