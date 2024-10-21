5d900f3c3ff3be33f516678e63d4ef5d08c2f072
Matej Hornik <bachini256@gmail.com>
2024-10-21 10:38:31 +0200

    sync script for downloading librispeech

--- run_download.sh
+++ run_download.sh
@@ -2,12 +2,12 @@
 
 export HF_HOME=/storage/brno2/home/xhorni20/.cache/huggingface
 
-python download_dataset.py \
+python scripts/download_dataset.py \
     --model_name_or_path="aaaaaaaaaaaaaa" \
     --output_dir="aaaaaaaaaaaaaaaaaaaaaa" \
-	--dataset_name="facebook/voxpopuli" \
-	--dataset_config_name="en" \
-	--train_split_name="train" \
+	--dataset_name="librispeech_asr" \
+	--dataset_config_name="clean" \
+	--train_split_name="train.100" \
 	--eval_split_name="validation" \
 	--preprocessing_num_workers="1" \
     --trust_remote_code \
