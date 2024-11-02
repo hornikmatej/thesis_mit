abeb1bd21fde6d1c397bcb2e3a474b68d52353c0
Matej Hornik <bachini256@gmail.com>
2024-11-02 14:03:39 +0100

    ctc args update & fixes

--- ctc/run_libri_ctc.sh
+++ ctc/run_libri_ctc.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 # -m torch.distributed.launch \
 # 	--nproc_per_node 8
-	
+	# --gradient_checkpointing \
 CUDA_VISIBLE_DEVICES="0" python run_speech_recognition_ctc.py \
 	--dataset_name="librispeech_asr" \
-	--model_name_or_path="facebook/wav2vec2-base" \
+	--model_name_or_path="facebook/wav2vec2-large-lv60" \
 	--dataset_config_name="clean" \
 	--train_split_name="train.100" \
 	--eval_split_name="validation" \
-	--output_dir="./wav2vec2base-librispeech-clean-100h" \
-	--preprocessing_num_workers="2" \
+	--output_dir="./wav2vec2-large-lv60-librispeech-clean-100h" \
+	--preprocessing_num_workers="4" \
 	--overwrite_output_dir \
 	--num_train_epochs="3" \
-	--per_device_train_batch_size="64" \
-	--per_device_eval_batch_size="64" \
-	--gradient_accumulation_steps="1" \
+	--per_device_train_batch_size="16" \
+	--per_device_eval_batch_size="16" \
+	--gradient_accumulation_steps="2" \
 	--learning_rate="3e-4" \
 	--warmup_steps="500" \
-	--evaluation_strategy="steps" \
+	--eval_strategy="steps" \
 	--text_column_name="text" \
 	--save_strategy="epoch" \
 	--eval_steps="100" \
