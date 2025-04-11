688009f255a23dea49bcbe82c72a3edc802633a6
Matej Hornik <bachini256@gmail.com>
2025-04-11 10:33:25 +0000

    upload: bash runner files

---
+++ run_scripts/run_libri_azure_32_512.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# python -m torch.distributed.launch --nproc_per_node 8 run_speech_recognition_seq2seq.py \
+CUDA_VISIBLE_DEVICES="0" python run_speech_recognition_seq2seq.py \
+	--dataset_name="librispeech_asr" \
+	--model_name_or_path="./seq2seq_wav2vec2_bart-base" \
+	--dataset_config_name="clean" \
+	--train_split_name="train.100" \
+	--eval_split_name="validation" \
+	--test_split_name="test" \
+	--output_dir="./seq2seq_wav2vec2_bart-base/librespeech_32_512" \
+	--preprocessing_num_workers="1" \
+	--dataloader_num_workers="16" \
+	--dataloader_prefetch_factor="2" \
+	--length_column_name="input_length" \
+	--overwrite_output_dir \
+	--num_train_epochs="5" \
+	--per_device_train_batch_size="64" \
+	--per_device_eval_batch_size="96" \
+	--gradient_accumulation_steps="1" \
+	--learning_rate="3e-4" \
+	--warmup_steps="400" \
+	--eval_strategy="steps" \
+	--text_column_name="text" \
+	--save_strategy="no" \
+	--eval_steps="2369" \
+	--logging_steps="10" \
+	--save_total_limit="1" \
+    --freeze_feature_encoder \
+	--bf16 \
+    --task="transcribe" \
+	--use_lora_adapter \
+	--lora_r="32" \
+	--lora_alpha="512" \
+	--lora_dropout="0.05" \
+	--lora_init="True" \
+	--predict_with_generate \
+	--do_train --do_eval --do_predict \
+	--do_lower_case \
+    --trust_remote_code \
+    --report_to="wandb" \
+	--sclite_path="/home/azureuser/media-disk/mh_dp/SCTK/bin/sclite" \
+	--wandb_project="seq2seq_encoder-decoder" \
+	--cache_dir="/home/azureuser/media-disk/mh_dp/preprocessed_dataset_libri" \
