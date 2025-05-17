c8743cfd0f48f043aed70ab2836d101dd78554d1
Matej Hornik <bachini256@gmail.com>
2025-05-17 09:56:19 +0000

    run scripts clean up

---
+++ run_scripts/voxpopuli_best.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES="0" python run_speech_recognition_seq2seq.py \
+	--dataset_name="facebook/voxpopuli" \
+	--model_name_or_path="./seq2seq_wav2vec2_bart-base_24k-en-voxpopuli" \
+	--dataset_config_name="en" \
+	--train_split_name="train" \
+	--eval_split_name="validation" \
+	--test_split_name="test" \
+	--output_dir="./seq2seq_wav2vec2_bart-base_24k-en-voxpopuli/t1_new1_spec" \
+	--preprocessing_num_workers="1" \
+	--dataloader_num_workers="16" \
+	--dataloader_prefetch_factor="2" \
+	--length_column_name="input_length" \
+	--overwrite_output_dir \
+	--num_train_epochs="20" \
+	--per_device_train_batch_size="96" \
+	--per_device_eval_batch_size="96" \
+	--gradient_accumulation_steps="1" \
+	--learning_rate="1e-4" \
+	--label_smoothing_factor="0.05" \
+	--apply_spec_augment \
+	--mask_time_prob="0.25" \
+	--mask_time_length="30" \
+	--mask_time_min_masks="2" \
+	--mask_feature_prob="0.3" \
+	--mask_feature_length="30" \
+	--mask_feature_min_masks="1" \
+	--weight_decay="0.01" \
+	--lr_scheduler_type="cosine_with_min_lr" \
+	--lr_scheduler_kwargs="{\"min_lr\": 5e-9}" \
+	--warmup_steps="2000" \
+	--eval_strategy="steps" \
+	--text_column_name="normalized_text" \
+	--save_strategy="steps" \
+	--eval_steps="1000" \
+	--save_steps="1000" \
+	--load_best_model_at_end \
+    --metric_for_best_model="wer" \
+    --greater_is_better=False \
+	--logging_steps="10" \
+	--save_total_limit="1" \
+    --freeze_feature_encoder \
+	--bf16 \
+    --task="transcribe" \
+	--predict_with_generate \
+	--do_train --do_eval --do_predict \
+	--do_lower_case \
+    --trust_remote_code \
+    --report_to="wandb" \
+	--sclite_path="/home/azureuser/media-disk/mh_dp/SCTK/bin/sclite" \
+	--wandb_project="seq2seq_encoder-decoder_vox" \
+	--cache_dir="/home/azureuser/media-disk/mh_dp/preprocessed_dataset_voxpopuli" \
